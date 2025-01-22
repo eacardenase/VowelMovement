eadf5ad741ffbcfe33bccd257c5c8aefdae75ed4
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-22 11:51:31 -0500

    Initial Commit

---
+++ VowelMovement/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VowelMovement
+//
+//  Created by Edwin Cardenas on 22/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
